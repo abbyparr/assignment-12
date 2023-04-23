6b9ad5c70bcba40c55e9f475561f7c82d48f7196
abbyparr <123125559+abbyparr@users.noreply.github.com>
2023-04-23 19:40:09 -0400

    Update assignment 12 q 1 .c

--- assignment 12 q 1 .c
+++ assignment 12 q 1 .c
@@ -1,10 +1,12 @@
 #open the file
 the_file = open('Stations.csv', 'r')
-print(the_file)
+  print(the_file)
 
 #map the code (IATA)
-char = the_file.read('K')
-print(char)
+#get B column only
+csvrow = [row[2] for row in reader]
+char = the_file.read('csvrow')
+  print(char)
 
 #close the file
 the_file.close()
